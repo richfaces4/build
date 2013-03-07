0a98656df35fe83479b38e9900a0a9a28270bc40
Lukas Fryc <lfryc@redhat.com>
2013-03-07 10:09:03 +0100

    bumping development version to 4.3.2-SNAPSHOT

--- arquillian/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
+++ arquillian/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
@@ -10,7 +10,7 @@ public class FundamentalTestConfiguration implements DroneConfiguration<Fundamen
 
     private String richfacesVersion;
     private Boolean servletContainerSetup;
-    private String currentBuildRichfacesVersion = "4.3.1-SNAPSHOT";
+    private String currentBuildRichfacesVersion = "4.3.2-SNAPSHOT";
     private String jsfImplementation;
     private String containerHome;
     private String containerDistribution;
